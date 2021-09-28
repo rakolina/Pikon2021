ff5b3025712c79a7f6d994150a16f1c019b0008e
Karolina Gustaitis <rakolina@gmail.com>
2021-09-28 19:41:47 +0300

    datetime isoformat date contains characters that crash writing to remote drive

--- pishutter.py
+++ pishutter.py
@@ -1,9 +1,10 @@
 #! /usr/bin/python
 
-# Continuosly monitor momentary button
+# Continuosly read from the momentary button
 # when pressed, get current date 
 # and create a new photo image, name it 
 # {timestamp}.jpg and save it in /home/pi
+# or on a removable USB drive if available
 
 import os
 from gpiozero import Button
@@ -11,24 +12,26 @@ from picamera import PiCamera
 from datetime import datetime
 from signal import pause
 
-HM = '/home/pi/Pictures'
-MEDIA = '/media/pi'
-
-media_dirs = os.listdir(MEDIA)
-if( len ( media_dirs ) ) > 0 and os.access( MEDIA + '/' + media_dirs[0],
-        os.W_OK ):
-    OUT = MEDIA
-else:
-    OUT = HM
+HM = '/home/pi/Pictures/'
+MEDIA = '/media/pi/PIKON/'
 
 camera = PiCamera()
 shutter_button = Button(20)
 
 def capture():
-    timestamp = datetime.now().isoformat()
-    camera.capture( OUT + '/%s.jpg' % timestamp )
+  
+    # check for USB drive
+    if( os.access( MEDIA, os.W_OK ) ):
+        OUT = MEDIA # write to removable drive
+    else:
+        OUT = HM # no USB drive write locally
 
+    # datetime contains special characters which fail removable drive writes
+    timestamp = datetime.now().strftime("%Y%m%d.%H%M%S") # strip all specials
 
+    filename = OUT + '%s.jpg' % timestamp 
+    camera.capture( filename )
+    
 
 shutter_button.when_pressed = capture
 
