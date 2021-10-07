dd85fc67757c94638ff01e8121734cd3da5964e0
pi <pi@pikon2021.lt>
2021-10-07 19:24:33 +0300

    added logging

--- pishutter.py
+++ pishutter.py
@@ -23,28 +23,37 @@ LOG = HM + 'pishutter.log'
 logging.basicConfig(filename=LOG, encoding='utf-8',
         format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
 
+logging.info( 'pishutter.py starting' )
+
 camera = PiCamera()
 shutter_button = Button( 20 )
 preview_button = Button( 21 )
 
-# check for a writable USB drive, use the first one found
-if( os.path.isdir ( MEDIA ) ):
-    mounted_dirs = os.listdir( MEDIA )
-    if( len( mounted_dirs ) > 0 and os.access( MEDIA + mounted_dirs[0], os.W_OK ) ):
-        OUT = MEDIA + mounted_dirs[0] + '/' # write to removable drive
-
-# read camera options configuration json file
-CONF = HM + 'pishutter.conf'
-if( os.path.isfile( CONF ) ):
-    with open(CONF, 'r') as jsonfile:
-        data = json.load(jsonfile)
-        jsonfile.close()
-else:
-    logger.error('Fatal error. Config file not found. Exiting')
-    exit( -1 )
-
 
 # FUNCTIONS    
+
+def setup_storage():
+
+    # check for a writable USB drive, use the first one found
+    if( os.path.isdir ( MEDIA ) ):
+        mounted_dirs = os.listdir( MEDIA )
+        if( len( mounted_dirs ) > 0 and os.access( MEDIA + mounted_dirs[0], os.W_OK ) ):
+            OUT = MEDIA + mounted_dirs[0] + '/' # write to removable drive
+    logging.info( 'writing pictures to ' + OUT )
+
+
+def read_config():
+
+    # read camera options configuration json file
+    CONF = HM + 'pishutter.conf'
+    if( os.path.isfile( CONF ) ):
+        with open(CONF, 'r') as jsonfile:
+            data = json.load(jsonfile)
+            jsonfile.close()
+    else:
+        logger.error('Fatal error. Config file not found. Exiting')
+        exit( -1 )
+
 
 def capture():
 
@@ -55,6 +64,9 @@ def capture():
     camera.capture( filename )
     logging.info( 'captured a still: ' + filename )
     
+
+setup_storage
+read_config
 
 preview_button.when_held = camera.start_preview
 preview_button.when_released = camera.stop_preview
