1f13bb9075ba94ba13cee1f2a9473304fca233eb
Karolina <pentatopas@gmail.com>
2022-01-20 23:40:50 +0200

    apply configuration options to camera instance

--- scripts/pishutter.py
+++ scripts/pishutter.py
@@ -14,33 +14,37 @@ from datetime import datetime
 from signal import pause
 
 HM = '/home/pi/Pikon2021/'
-CONF = HM + 'conf/pikon.v1.conf'
-OUT = '/home/pi/Pictures/'
-DEBUG = 1
+CONF = HM + 'conf/pikon.conf'
+LCL_OUT = '/home/pi/Pictures/'
 
+# Expected configuratio options
+RAW = ['yuv', 'rgb', 'rgba', 'bgr', 'bgra']
+ENCODED = ['jpeg', 'gif', 'bmp', 'png']
+ISO = ['100', '200', '300', '400', '500', '600', '700', '800']
+SHUTTER = [0.000001, 10]
+
+# def capture(self, output, format=None, use_video_port=False, resize=None, splitter_port=0, bayer=False, **options):
 def capture():
     # datetime contains special characters which fail removable drive writes
     timestamp = datetime.now().strftime("%Y%m%d.%H%M%S") # strip all specials
+    filename = LCL_OUT + '%s' % timestamp + '.' + conf_data["format"]
+    if conf_data["format"] in ENCODED:
+        camera.iso = conf_data["iso"]
+    camera.shutter_speed = conf_data["shutter"]
+    camera.capture(filename, format=conf_data["format"])
 
-    filename = '%s.jpg' % timestamp 
-    camera.capture( OUT + filename )
-    if ( 1 == DEBUG ):
-        print ( 'saving: ' + filename )
-    
 
-# read camera options configuration json file
-if( os.path.isfile( CONF ) ):
-    with open( CONF, 'r' ) as jsonfile:
-        data = json.load(jsonfile)
-        jsonfile.close()
-else:
+if not os.path.isfile(CONF):
     print ('Fatal. Config file not found. Exit')
     exit( -1 )
-
-# TODO validate config options
+else:
+    with open( CONF, 'r' ) as jsonfile:
+        conf_data = json.load(jsonfile)
+        jsonfile.close()
 
 
 camera = PiCamera()
+
 shutter_button = Button( 20 )
 preview_button = Button( 21 )
 
